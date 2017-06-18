7c544610c8f07bc7e7e6718ee8fabf4c69c77ecb
soonboylena <17131007@qq.com>
2017-06-18 09:22:23 +0800

    配置文件（用于测试）

---
+++ seven-config-client/src/main/java/com/yushi/sunb/SevenConfigClientApplication.java
@@ -0,0 +1,12 @@
+package com.yushi.sunb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SevenConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SevenConfigClientApplication.class, args);
+	}
+}
