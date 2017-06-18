aeca20d93b849bd65e313754d7396368dba662e2
soonboylena <17131007@qq.com>
2017-06-18 09:42:24 +0800

    config service连接注册中心

---
+++ seven-config-client/src/main/java/com/yushi/sunb/SevenConfigClientApplication.java
@@ -0,0 +1,14 @@
+package com.yushi.sunb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by sunb on 2017/6/18.
+ */
+@SpringBootApplication
+public class SevenConfigClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SevenConfigClientApplication.class, args);
+    }
+}
